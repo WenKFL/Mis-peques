d2345be84e8f07504a90358bdf219bcc3d39fbee
WenKFL <wkocfong@gmail.com>
2026-09-15 17:53:15 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,1 @@
+const C='mp-v2';self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(['./','./index.html','./manifest.webmanifest','./icon-180.png']))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))))
